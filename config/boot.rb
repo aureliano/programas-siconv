c456fd133df31ddc27ce0606b92f056460d599a2
aureliano <aureliano.franca@hotmail.com>
2012-11-13 20:50:41 -0200

    Carrega no boot da aplicação a data da última extração de dados.

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,7 @@
 # Defines our constants
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
+LAST_EXTRACTION_DATE = File.read('.data_extraction_date').strip
 
 # Load our dependencies
 require 'rubygems' unless defined?(Gem)
