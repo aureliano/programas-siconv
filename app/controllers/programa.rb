e24b9c7fa789b2bf0130069d7896b9a67638d476
aureliano <aureliano.franca@hotmail.com>
2012-11-28 21:20:39 -0200

    Cria página de detalhamento do programa.

--- app/controllers/programa.rb
+++ app/controllers/programa.rb
@@ -1,22 +1,8 @@
 ProgramasSiconv.controllers :programa do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
-
+  
+  get :programa, :map => '/programa/:codigo' do
+    @programa = Programa.first(:cod_programa_siconv => params[:codigo])
+    render 'programa/programa'
+  end
   
 end
