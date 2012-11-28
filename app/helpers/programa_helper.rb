7596492d433f709ecbcc69d3c0f6f75886a442b6
aureliano <aureliano.franca@hotmail.com>
2012-11-28 21:20:07 -0200

    Cria métodos utilitários para formatação de datas e booleanos.

--- app/helpers/programa_helper.rb
+++ app/helpers/programa_helper.rb
@@ -1,7 +1,22 @@
+# encoding: utf-8
+
 ProgramasSiconv.helpers do
 
   def time_to_date_s(time)
-    return '' if time.nil?
+    return '-' if time.nil?
     time.strftime '%d/%m/%Y'
+  end
+  
+  def format_date(date_str)
+    return '-' if date_str.nil? || date_str.empty?
+    date_str.gsub /-/, '/'
+  end
+  
+  def parse_boolean(bool)
+    case bool
+      when true; then 'Sim'
+      when false; then 'Não'
+      when nil; then 'Não'
+    end
   end
 end
