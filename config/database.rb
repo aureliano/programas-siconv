42ac85fff559a881e1ae9d5074af304084fda7ba
aureliano <aureliano.franca@hotmail.com>
2012-11-30 14:29:05 -0200

    Altera setup do banco de dados para produção.

--- config/database.rb
+++ config/database.rb
@@ -3,6 +3,8 @@ DataMapper::Property::String.length(255)
 
 case Padrino.env
   when :development then DataMapper.setup(:default, :adapter => 'mongo', :database => 'programas_siconv')
-  when :production then DataMapper.setup(:default, :adapter => 'mongo', :database => 'programas_siconv')
+  when :production then DataMapper.setup(:default, ENV['MONGOLAB_URI'])
   when :test then DataMapper.setup(:default, :adapter => 'mongo', :database => 'programas_siconv')
 end
+
+# mongodb://<dbuser>:<dbpassword>@ds033187.mongolab.com:33187/heroku_app9631427
