5efc4f6462e4d1e222612b7650aa194388c79020
aureliano <aureliano.franca@hotmail.com>
2012-11-30 14:53:01 -0200

    Altera configurações.

--- config.ru
+++ config.ru
@@ -4,6 +4,7 @@
 # This file can be used to start Padrino,
 # just execute it from the command line.
 
+require 'dm-mongodb-adapter'
 require File.expand_path("../config/boot.rb", __FILE__)
 
 run Padrino.application
--- config/database.rb
+++ config/database.rb
@@ -6,5 +6,3 @@ case Padrino.env
   when :production then DataMapper.setup(:default, ENV['MONGOLAB_URI'])
   when :test then DataMapper.setup(:default, :adapter => 'mongo', :database => 'programas_siconv')
 end
-
-# mongodb://<dbuser>:<dbpassword>@ds033187.mongolab.com:33187/heroku_app9631427
