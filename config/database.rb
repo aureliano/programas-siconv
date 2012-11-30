c5a26f351c245bf47ad0cfe71dd9b7c0aaa91254
aureliano <aureliano.franca@hotmail.com>
2012-11-30 18:50:58 -0200

    Ignora código do arquivo de configuração padrão de base de dados.

--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,8 @@
 # URI format => mongodb://<dbuser>:<dbpassword>@ds033187.mongolab.com:33187/heroku_app9631427
 # Utilizando código do método before_load em boot.rb
 
-case Padrino.env
-  when :development then Mongo::MongoClient.new('localhost', 27017)['programas_siconv']
-  when :production then Mongo::MongoClient.from_uri(ENV['MONGOLAB_URI'])['programas_siconv']
-  when :test then Mongo::MongoClient.new('localhost', 27017)['programas_siconv']
-end
+#case Padrino.env
+#  when :development then Mongo::MongoClient.new('localhost', 27017)['programas_siconv']
+#  when :production then Mongo::MongoClient.from_uri(ENV['MONGOLAB_URI'])
+#  when :test then Mongo::MongoClient.new('localhost', 27017)['programas_siconv']
+#end
