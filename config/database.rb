5d0b51e92d584b182d76f68e546feccbe849b7e6
aureliano <aureliano.franca@hotmail.com>
2013-01-23 17:22:06 -0200

    Corrige nome da base de dados de produção.

--- config/database.rb
+++ config/database.rb
@@ -3,9 +3,9 @@ host = 'localhost'
 port = Mongo::Connection::DEFAULT_PORT
 
 database_name = case Padrino.env
-  when :development then 'convenios_siconv_development'
-  when :production  then 'heroku_app10168188'
-  when :test        then 'convenios_siconv_test'
+  when :development then 'programas_siconv_development'
+  when :production  then 'heroku_app9631427'
+  when :test        then 'programas_siconv_test'
 end
 
 Mongoid.database = if Padrino.env == :production
